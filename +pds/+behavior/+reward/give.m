fdffc96b1722c001fc03f5285db9ba52bd4391fb
Rig1 User <rig1_user@018>
2017-02-24 10:40:03 -0600

    WZ: start implementing default event codes

--- +pds/+behavior/+reward/give.m
+++ +pds/+behavior/+reward/give.m
@@ -14,18 +14,20 @@ function p = give(p, amount)
     end
 
     if(p.trial.datapixx.use && p.trial.datapixx.useForReward)
-    %% datapixx analog output is used
+        %% datapixx analog output is used
         pds.datapixx.analogOut(amount, p.trial.datapixx.adc.RewardChannel, p.trial.datapixx.adc.TTLamp);
     elseif(p.trial.newEraSyringePump.use)
-    %% Era Syringe Pump is used
+        %% Era Syringe Pump is used
         pds.newEraSyringePump.give(p,amount);
     end
 
+    % send event code for reward    
     pds.datapixx.flipBit(p.trial.event.REWARD);
 
     %%sound
     if(p.trial.sound.use && p.trial.sound.useForReward)
         PsychPortAudio('Start', p.trial.sound.reward);
+        pds.datapixx.flipBit(p.trial.event.AUDIO_REW);
     end
     
     %% store data
